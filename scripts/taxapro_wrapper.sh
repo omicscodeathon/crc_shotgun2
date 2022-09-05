2715df476fd13de6716fea8b9f380ce7cad4c99b
Olaitan I. Awe <laitanawe@gmail.com>
2022-09-05 21:28:50 +0100

    Update scripts

--- scripts/taxapro_wrapper.sh
+++ scripts/taxapro_wrapper.sh
@@ -7,7 +7,10 @@ cpus=28
 ram=88
 
 #Create variables
-forward=$(ls ../fastqs/crc_old/SRR1612431[12]*_1.fastq)
+forward=$(ls ../fastqs/crc_old/*_1.fastq)
+#forward1=$(ls ../fastqs/crc_old/SRR1612431[12]*_1.fastq)
+#forward2=$(ls ../fastqs/crc_old/SRR1612431[569]*_1.fastq)
+#forward3=$(ls ../fastqs/crc_old/SRR1612432[023]*_1.fastq)
 taxapro_out="taxapro_outdir"
 
 #Run spades
@@ -20,6 +23,7 @@ do
         mkdir -pv ${taxapro_out}/${samplename}
         rev=$(echo $fwd | sed 's/_1/_2/')
         python3 taxapro_install/taxapro.py -f ${fwd} -r ${rev} -o ${taxapro_out}/${samplename}
+	mv -fv ${taxapro_out}/${samplename}/taxa_stat/taxa_freq.csv ${taxapro_out}/${samplename}/taxa_stat/${samplename}.taxa_freq.csv
 done
 
 echo "Analysis with Taxapro Pipeline is now completed for all metagenomes!"
