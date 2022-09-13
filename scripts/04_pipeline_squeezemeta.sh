7c55c96233d9cdfb0e48edf24784ae83c23f9f09
Olaitan I. Awe <laitanawe@gmail.com>
2022-09-13 11:43:58 +0100

    Update scripts

--- scripts/04_pipeline_squeezemeta.sh
+++ scripts/04_pipeline_squeezemeta.sh
@@ -5,9 +5,9 @@
 # Dependencies: SqueezeMeta
 
 cpus=30
-ram=120
+#ram=120
 # When RAM is specified in the command, SqueezeMeta expects an Integer which is the available RAM divided by 8
-ram_squeezemeta=$(expr $ram \/ 8)
+#ram_squeezemeta=$(expr $ram \/ 8)
 
 #Create variables
 
@@ -30,10 +30,10 @@ healthy_old_dir=${squeezemeta_dir}/healthy_old
 healthy_young_dir=${squeezemeta_dir}/healthy_young
 
 # Run the analysis command using SqueezeMeta on group1
-/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -b $ram_squeezemeta -m coassembly -p $patient_old_dir -s $infile1 -f $fastq_dir
+#/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -m coassembly -p $patient_old_dir -s $infile1 -f $fastq_dir
 
 # Run the analysis command using SqueezeMeta on group2
-#/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -b $ram_squeezemeta -m coassembly -p $healthy_old_dir -s $infile2 -f $fastq_dir
+/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -m coassembly -p $healthy_old_dir -s $infile2 -f $fastq_dir
 
 # Run the analysis command using SqueezeMeta on group3
 #/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -b $ram_squeezemeta -m coassembly -p $patient_young_dir -s $infile3 -f $fastq_dir
@@ -41,4 +41,4 @@ healthy_young_dir=${squeezemeta_dir}/healthy_young
 # Run the analysis command using SqueezeMeta on group4
 #/opt/SqueezeMeta-1.5.2/scripts/SqueezeMeta.pl -t $cpus -b $ram_squeezemeta -m coassembly -p $healthy_young_dir -s $infile4 -f $fastq_dir
 
-echo "Shotgun Metagenomic data analysis with SqueezeMeta completed for all samples"
+echo "Shotgun Metagenomic data analysis with SqueezeMeta completed for all healthy old samples"
